13a37e8652870f50ca62c7b5c2476e3a5c002be8
bsametarman@outlook.com <bsametarman@outlook.com>
2025-08-19 20:08:07 +0300

    feat(page): add home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default HomePage;
